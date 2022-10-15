d614fe8d0826fd84f70ad7dcc54052c136020233
Cuynu <90895715+cuynu@users.noreply.github.com>
2022-10-15 20:51:39 +0700

    Update uTLS and v2rayNG 1.7.21 code

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/dto/V2rayConfig.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/dto/V2rayConfig.kt
@@ -89,6 +89,7 @@ data class V2rayConfig(
                                  var users: List<UsersBean>) {
 
                 data class UsersBean(var id: String = "",
+                                     var alterId: Int? = null,
                                      var security: String = DEFAULT_SECURITY,
                                      var level: Int = DEFAULT_LEVEL,
                                      var encryption: String = "",
@@ -102,7 +103,7 @@ data class V2rayConfig(
                                    var port: Int = DEFAULT_PORT,
                                    var level: Int = DEFAULT_LEVEL,
                                    val email: String? = null,
-                                   val flow: String? = null,
+                                   var flow: String? = null,
                                    val ivCheck: Boolean? = null,
                                    var users: List<SocksUsersBean>? = null) {
 
@@ -258,15 +259,18 @@ data class V2rayConfig(
                 return sni
             }
 
-            fun populateTlsSettings(streamSecurity: String, allowInsecure: Boolean, sni: String) {
+            fun populateTlsSettings(streamSecurity: String, allowInsecure: Boolean, sni: String, fingerprint: String?) {
                 security = streamSecurity
                 val tlsSetting = TlsSettingsBean(
-                        allowInsecure = allowInsecure,
-                        serverName = sni
+                    allowInsecure = allowInsecure,
+                    serverName = sni,
+                    fingerprint = fingerprint
                 )
                 if (security == TLS) {
                     tlsSettings = tlsSetting
+                    xtlsSettings = null
                 } else if (security == XTLS) {
+                    tlsSettings = null
                     xtlsSettings = tlsSetting
                 }
             }
@@ -322,7 +326,8 @@ data class V2rayConfig(
 
         fun getTransportSettingDetails(): List<String>? {
             if (protocol.equals(EConfigType.VMESS.name, true)
-                    || protocol.equals(EConfigType.VLESS.name, true)) {
+                    || protocol.equals(EConfigType.VLESS.name, true)
+                    || protocol.equals(EConfigType.TROJAN.name, true)) {
                 val transport = streamSettings?.network ?: return null
                 return when (transport) {
                     "tcp" -> {
@@ -383,7 +388,7 @@ data class V2rayConfig(
     }
 
     data class RoutingBean(var domainStrategy: String,
-                           val domainMatcher: String? = null,
+                           var domainMatcher: String? = null,
                            var rules: ArrayList<RulesBean>,
                            val balancers: List<Any>? = null) {
 
